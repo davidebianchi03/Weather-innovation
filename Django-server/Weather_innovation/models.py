1a69b91fefa893bfaadf75de2cb8064f53d1a66e
DESKTOP-PT4JFDP\Davide <dadebianchi2003@gmail.com>
2021-08-11 14:25:04 +0200

    no message

--- Django-server/Weather_innovation/models.py
+++ Django-server/Weather_innovation/models.py
@@ -19,7 +19,17 @@ class Surveys(models.Model):
     temperature = models.FloatField()
     pressure = models.FloatField()
     humidity = models.FloatField()
+    device_id = models.CharField(max_length=5, default='00000')
+    time_in_millis = models.IntegerField(default=0)
 
     class Meta:
         verbose_name = 'Surveys'
         verbose_name_plural = 'Surveys'
+
+class UsersDevices(models.Model):
+    username = models.CharField(max_length=50)
+    device_id = models.CharField(max_length=5)
+
+    class Meta:
+        verbose_name = 'UsersDevices'
+        verbose_name_plural = 'UsersDevices'
--- Django-server/Weather_innovation/views.py
+++ Django-server/Weather_innovation/views.py
@@ -1,28 +1,76 @@
 from django.http import response
-from Weather_innovation.models import Surveys
+from Weather_innovation.models import Surveys, UsersDevices
 from django.http.response import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+import time
 
 # Create your views here.
-class IndexView(generic.TemplateView):
-    template_name = 'Weather_innovation/index.html'
+@login_required(login_url='/login')
+def IndexView(request):
+    return render(request=request, template_name = 'Weather_innovation/index.html')
 
 @csrf_exempt
 def getSurveysView(request):
-    
-    response = list()
+	response = list()
+	devices_list = UsersDevices.objects.filter(username = request.user.username).values_list('device_id', flat=True) 
+	for device in devices_list:
+		surveys_id_list = Surveys.objects.filter(device_id = device).values_list('id', flat=True)
+		for i in surveys_id_list:
+			data = Surveys.objects.get(id = i)
+			s_longitude = data.longitude
+			s_latitude = data.latitude
+			s_temperature = data.temperature
+			s_pressure = data.pressure
+			s_humidity = data.humidity
+			s_time = data.time
+			response.append({'longitude':s_longitude,'latitude':s_latitude,'temperature':s_temperature,'pressure':s_pressure,'humidity':s_humidity,'time':s_time})
 
-    surveys_id_list = Surveys.objects.values_list('id', flat=True)
-    for i in surveys_id_list:
-        data = Surveys.objects.get(id = i)
-        s_longitude = data.longitude
-        s_latitude = data.latitude
-        s_temperature = data.temperature
-        s_pressure = data.pressure
-        s_humidity = data.humidity
-        s_time = data.time
-        response.append({'longitude':s_longitude,'latitude':s_latitude,'temperature':s_temperature,'pressure':s_pressure,'humidity':s_humidity,'time':s_time})
+	return JsonResponse(response,safe=False)
 
-    return JsonResponse(response,safe=False)
+@csrf_exempt
+def getLatestSurveysView(request):
+	response = list()
+	devices_list = UsersDevices.objects.filter(username = request.user.username).values_list('device_id', flat=True) 
+	for device in devices_list:
+		data = Surveys.objects.filter(device_id = device).last()
+		if round(time.time() * 1000) - data.time_in_millis < 3600000:
+			s_longitude = data.longitude
+			s_latitude = data.latitude
+			s_temperature = data.temperature
+			s_pressure = data.pressure
+			s_humidity = data.humidity
+			s_time = data.time
+			response.append({'longitude':s_longitude,'latitude':s_latitude,'temperature':s_temperature,'pressure':s_pressure,'humidity':s_humidity,'time':s_time})
+
+	return JsonResponse(response,safe=False)
+
+@csrf_exempt
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("Weather_innovation:index")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="Weather_innovation/login.html", context={"login_form":form})
+
+def logout_view(request):
+	logout(request)
+	messages.info(request, "Logged out successfully!")
+	return redirect("Weather_innovation:index")
