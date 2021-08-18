30301160995773cc3717857162189005ea98921d
DESKTOP-PT4JFDP\Davide <dadebianchi2003@gmail.com>
2021-08-18 14:25:39 +0200

    Creata prima versione dell'applicazione per android

--- Arduino/Recivedataclient/env_settings.h
+++
@@ -1,6 +0,0 @@
-#include "Arduino.h"
-
-#define WIFI_SSID "zyxel"
-#define WIFI_PASSWORD "mauro1969"
-const String SERVER_PATH = "http://weatherinnovation.pythonanywhere.com";
-#define DEVICE_ID "a0001"
